61c51a04c115ac35f67004ddaf6de97487d22987
Braden Mars <BradenM@users.noreply.github.com>
2019-11-02 16:57:46 -0500

    feat(fonts): Install Patched Fonts directly from Git
    Fetch and install them from repo

--- dotfiles/config/fontconfig/fonts/install.sh
+++ dotfiles/config/fontconfig/fonts/install.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
+# Load Scripts
+source "/home/${SUDO_USER}/.scripts/git.sh"
+
+# Nerd Fonts to Install
+FONTS=("FiraCode")
+
+# Fetch
+mkdir /tmp/install_fonts
+for font in "${FONTS[$@]}"; do
+    dest="/tmp/install_fonts/${font}"
+    src="patched-fonts/${font}"
+    git_dir "ryanoasis/nerd-fonts" "$src" "$dest"
+done
+
+pushd /tmp/install_fonts
+
+# Install
 echo Installing fonts...
-find . -iname "*.otf" -not -iname "*Windows Compatible.otf" -execdir install -Dm644 {} "$pkgdir/usr/share/fonts/OTF/{}" \;
-find . -iname "*.ttf" -not -iname "*Windows Compatible.ttf" -execdir install -Dm644 {} "$pkgdir/usr/share/fonts/TTF/{}" \;
+find . -iname "*.otf" -not -iname "*Windows Compatible.otf" -print0 -execdir install -Dm644 {} "$pkgdir/usr/share/fonts/OTF/{}" \;
+find . -iname "*.ttf" -not -iname "*Windows Compatible.ttf" -print0 -execdir install -Dm644 {} "$pkgdir/usr/share/fonts/TTF/{}" \;
+
+popd
+
+# Cleanup
+rm -rf /tmp/install_fonts
 
 echo Done!
