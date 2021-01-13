0d6d7bfd4823c0ee7086659a7f40780e96bc372f
Braden Mars <bradenmars@bradenmars.me>
2021-01-13 02:10:41 -0600

    chore(sway): batch sway/waybar update

---
+++ dotfiles/config/sway/scripts/autotiling.sh
@@ -0,0 +1,11 @@
+
+#!/usr/bin/env sh
+
+# Terminate already running autotiling instances
+killall -q autotiling
+
+# Wait until the processes have been shut down
+while pgrep -x autotiling >/dev/null; do sleep 1; done
+
+# Launch
+autotiling
--- dotfiles/config/waybar/modules/spotify.sh
+++ dotfiles/config/waybar/modules/spotify.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-#class=$(playerctl metadata --player=spotify --format '{{lc(status)}}')
-class=$(mpc status | awk '{if (NR == 2) print $1}' | tr -d '[]')
+class=$(playerctl metadata --player=spotify --format '{{lc(status)}}')
+#class=$(mpc status | awk '{if (NR == 2) print $1}' | tr -d '[]')
 icon=""
 
 if [[ $class == "playing" ]]; then
-  info=$(mpc current)
+  #info=$(mpc current)
+  info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
   if [[ ${#info} > 40 ]]; then
     info=$(echo $info | cut -c1-40)"..."
   fi
