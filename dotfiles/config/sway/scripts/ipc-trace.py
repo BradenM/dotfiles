1a287b7b381a858159547ea15311c60b34e99b89
Braden Mars <bradenmars@bradenmars.me>
2022-07-20 15:32:51 -0500

    feat(sway): update and at binding event to ipc-trace debug script

--- dotfiles/config/sway/scripts/ipc-trace.py
+++ dotfiles/config/sway/scripts/ipc-trace.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 import i3ipc
+from i3ipc import Event
 from time import strftime, gmtime
+
+"""Simple IPC debugging tool extended from example in docs."""
 
 i3 = i3ipc.Connection()
 
 
 def print_separator():
-    print('-----')
+    print("-----")
 
 
 def print_time():
@@ -16,34 +19,42 @@ def print_time():
 
 def print_con_info(con):
     if con:
-        print('Id: %s' % con.id)
-        print('Name: %s' % con.name)
+        print("Id: %s" % con.id)
+        print("Name: %s" % con.name)
     else:
-        print('(none)')
+        print("(none)")
 
 
 def on_window(i3, e):
     print_separator()
-    print('Got window event:')
+    print("Got window event:")
     print_time()
-    print('Change: %s' % e.change)
+    print("Change: %s" % e.change)
     print_con_info(e.container)
 
 
 def on_workspace(i3, e):
     print_separator()
-    print('Got workspace event:')
+    print("Got workspace event:")
     print_time()
-    print('Change: %s' % e.change)
-    print('Current:')
+    print("Change: %s" % e.change)
+    print("Current:")
     print_con_info(e.current)
-    print('Old:')
+    print("Old:")
     print_con_info(e.old)
 
 
-# TODO subscribe to all events
+def on_binding(i3, e):
+    print_separator()
+    print("Got binding event:")
+    print_time()
+    print(f"Event: {e.change}")
+    keybind = f"{'+'.join(e.binding.event_state_mask)}+{e.binding.symbol}"
+    print(f'Binding: "{e.binding.command}" via "{keybind}"')
 
-i3.on('window', on_window)
-i3.on('workspace', on_workspace)
+
+i3.on(Event.WINDOW, on_window)
+i3.on(Event.WORKSPACE, on_workspace)
+i3.on(Event.BINDING, on_binding)
 
 i3.main()
