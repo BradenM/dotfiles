bcfb377873dc9a163b4830b0bc4951271ac3572f
Braden Mars <bradenmars@bradenmars.me>
2023-02-06 03:09:58 -0600

    feat(nvim): update nvim config
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- dotfiles/config/nvim/lua/user/init.lua
+++ dotfiles/config/nvim/lua/user/init.lua
@@ -38,11 +38,22 @@ local config = {
   options = {
     opt = {
       relativenumber = true, -- sets vim.opt.relativenumber
+      number = true,
+      spell = false,
+      signcolumn = "auto",
+      wrap = false
     },
     g = {
       mapleader = " ", -- sets vim.g.mapleader
       sonokai_style = "shusia", -- set sonokai colorscheme variant.
       suda_smart_edit = 1, -- enable suda auto buffer switch.
+      cmp_enabled = true, -- enable completion at start
+      autopairs_enabled = true, -- enable autopairs at start
+      diagnostics_enabled = true, -- enable diagnostics at start
+      status_diagnostics_enabled = true, -- enable diagnostics in statusline
+      icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+      ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+      heirline_bufferline = true, -- enable new heirline based bufferline (requires :PackerSync after changing)
       -- python3_host_prog = "/home/bradenmars/.pyenv/versions/neovim/bin/python" -- nvim python executable.
     },
   },
@@ -76,10 +87,10 @@ local config = {
   },
 
   -- Disable AstroNvim ui features
-  ui = {
-    nui_input = true,
-    telescope_select = true,
-  },
+  -- ui = {
+  --   nui_input = true,
+  --   telescope_select = true,
+  -- },
 
   -- Configure plugins
   plugins = {
@@ -111,36 +122,36 @@ local config = {
       -- },
       -- Code refactoring.
       { "ThePrimeagen/refactoring.nvim" },
-      -- DAP
-      { "mfussenegger/nvim-dap",
-        config = function()
-          local dap = require("dap")
-          dap.adapters.node2 = {
-            type = 'executable',
-            command = 'node',
-            args = { os.getenv('HOME') .. '/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js' },
-          }
-          dap.configurations.javascript = {
-            {
-              name = 'Launch',
-              type = 'node2',
-              request = 'launch',
-              program = '${file}',
-              cwd = vim.fn.getcwd(),
-              sourceMaps = true,
-              protocol = 'inspector',
-              console = 'integratedTerminal',
-            },
-            {
-              -- For this to work you need to make sure the node process is started with the `--inspect` flag.
-              name = 'Attach to process',
-              type = 'node2',
-              request = 'attach',
-              processId = require 'dap.utils'.pick_process,
-            },
-          }
-        end
-      },
+      -- DAP (included in astronvim now?)
+      -- { "mfussenegger/nvim-dap",
+      --   config = function()
+      --     local dap = require("dap")
+      --     -- dap.adapters.node2 = {
+      --     --   type = 'executable',
+      --     --   command = 'node',
+      --     --   args = { os.getenv('HOME') .. '/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js' },
+      --     -- }
+      --     dap.configurations.javascript = {
+      --       {
+      --         name = 'Launch',
+      --         type = 'node2',
+      --         request = 'launch',
+      --         program = '${file}',
+      --         cwd = vim.fn.getcwd(),
+      --         sourceMaps = true,
+      --         protocol = 'inspector',
+      --         console = 'integratedTerminal',
+      --       },
+      --       {
+      --         -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+      --         name = 'Attach to process',
+      --         type = 'node2',
+      --         request = 'attach',
+      --         processId = require 'dap.utils'.pick_process,
+      --       },
+      --     }
+      --   end
+      -- },
     },
     -- All other entries override the setup() call for default plugins
     ["null-ls"] = function(config)
@@ -155,6 +166,7 @@ local config = {
         -- Formatters
         null_ls.builtins.formatting.rubocop,
         null_ls.builtins.formatting.black.with(pyvenv_local),
+        null_ls.builtins.formatting.isort.with(pyvenv_local),
         -- Linters
         -- null_ls.builtins.diagnostics.actionlint,
         null_ls.builtins.diagnostics.hadolint,
@@ -202,14 +214,23 @@ local config = {
       return config -- return final config table
     end,
     treesitter = {
-      ensure_installed = { "lua" },
+      -- ensure_installed = { "lua" },
     },
-    ["nvim-lsp-installer"] = {
-      ensure_installed = { "sumneko_lua" },
+    -- use mason-lspconfig to configure LSP installations
+    ["mason-lspconfig"] = { -- overrides `require("mason-lspconfig").setup(...)`
+      -- ensure_installed = { "sumneko_lua" },
     },
-    packer = {
-      compile_path = vim.fn.stdpath "data" .. "/packer_compiled.lua"
+    -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+    ["mason-null-ls"] = { -- overrides `require("mason-null-ls").setup(...)`
+      -- ensure_installed = { "prettier", "stylua" },
     },
+    ["mason-nvim-dap"] = { -- overrides `require("mason-nvim-dap").setup(...)`
+      -- ensure_installed = { "python" },
+    },
+
+    -- packer = {
+    --   compile_path = vim.fn.stdpath "data" .. "/packer_compiled.lua"
+    -- },
   },
 
   -- LuaSnip Options
@@ -252,17 +273,62 @@ local config = {
     },
   },
 
+  -- Customize Heirline options
+  heirline = {
+    -- -- Customize different separators between sections
+    -- separators = {
+    --   tab = { "", "" },
+    -- },
+    -- -- Customize colors for each element each element has a `_fg` and a `_bg`
+    -- colors = function(colors)
+    --   colors.git_branch_fg = astronvim.get_hlgroup "Conditional"
+    --   return colors
+    -- end,
+    -- -- Customize attributes of highlighting in Heirline components
+    -- attributes = {
+    --   -- styling choices for each heirline element, check possible attributes with `:h attr-list`
+    --   git_branch = { bold = true }, -- bold the git branch statusline component
+    -- },
+    -- -- Customize if icons should be highlighted
+    -- icon_highlights = {
+    --   breadcrumbs = false, -- LSP symbols in the breadcrumbs
+    --   file_icon = {
+    --     winbar = false, -- Filetype icon in the winbar inactive windows
+    --     statusline = true, -- Filetype icon in the statusline
+    --   },
+    -- },
+  },
+
   -- Extend LSP configuration
   lsp = {
     -- enable servers that you already have installed without lsp-installer
     servers = {
-      "pyright"
+      -- "pyright"
     },
     -- easily add or disable built in mappings added during LSP attaching
     mappings = {
       n = {
         -- ["<leader>lf"] = false -- disable formatting keymap
       },
+    },
+    formatting = {
+      -- control auto formatting on save
+      format_on_save = {
+        enabled = true, -- enable or disable format on save globally
+        allow_filetypes = { -- enable format on save for specified filetypes only
+          -- "go",
+        },
+        ignore_filetypes = { -- disable format on save for specified filetypes
+          -- "python",
+        },
+      },
+      disabled = { -- disable formatting capabilities for the listed language servers
+        -- "sumneko_lua",
+      },
+      timeout_ms = 1000, -- default format timeout
+      -- filter = function(client) -- fully override the default formatting function
+      --   return true
+      -- end
     },
     -- add to the server on_attach function
     -- on_attach = function(client, bufnr)
@@ -327,8 +393,13 @@ local config = {
   mappings = {
     -- first key is the mode
     n = {
+      ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+      ["<leader>bc"] = { "<cmd>BufferLinePickClose<cr>", desc = "Pick to close" },
+      ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
+      ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
       -- second key is the lefthand side of the map
       ["<C-s>"] = { ":w!<cr>", desc = "Save File" },
+      ["<C-n>"] = { ":nohl<cr>", desc = "Unhighlight all" }
     },
     t = {
       -- setting a mapping to false will disable it
