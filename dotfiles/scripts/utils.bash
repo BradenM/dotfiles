d33def41621b0b82c192d85df091c56bc2127a91
Braden Mars <bradenmars@bradenmars.me>
2023-02-06 03:12:26 -0600

    feat(scripts): update script utils
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- dotfiles/scripts/utils.bash
+++ dotfiles/scripts/utils.bash
@@ -39,17 +39,39 @@ fail() {
 log_title() {
 	local -A c
 	make_colors c
-	printf "\n%s%s%s\n" "${c['bright_white']}${c['under']}" "$*" "${c['reset']}"
+	printf "\n%s%s%s\n" "${c['bright_white']}${c['under']}" "$*" "${c['reset']}" 1>&2
+}
+
+log_subtitle() {
+	local -A c
+	make_colors c
+	printf "\n%s%s%s\n" "${c['white']}" "$*" "${c['reset']}" 1>&2
 }
 
 log_info() {
 	local -A c
 	make_colors c
-	printf "%s%s%s\n" "${c['cyan']}" "$*" "${c['reset']}"
+	printf "%s%s%s\n" "${c['cyan']}" "$*" "${c['reset']}" 1>&2
 }
 
 log_success() {
 	local -A c
 	make_colors c
-	printf "%s%s%s\n" "${c['bold']}${c['green']}" "$*" "${c['reset']}"
+	printf "%s%s%s\n" "${c['bold']}${c['green']}" "$*" "${c['reset']}" 1>&2
+}
+
+log_header() {
+	local -A c
+	local len line1len line2len bar
+	make_colors c
+	line1len=$(echo -n "$1" | wc -c)
+	line2len=$(echo -n "$2" | wc -c)
+	len=$line2len
+	[ $line1len -gt $line2len ] && len=$line1len
+	len=$((len + 2))
+	bar=$(printf "%${len}s" | tr ' ' '*')
+	echo "$bar" 1>&2
+	echo -n " ${c[bright_white]}${1}${c[reset]} "
+	[ "$2" ] && log_subtitle " ${c[white]}${c[dim]}$2${c[reset]} "
+	echo "$bar" 1>&2
 }
