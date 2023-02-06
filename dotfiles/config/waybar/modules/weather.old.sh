5bc9a0ed481bc81bc53957a4ac3c69940c8a82f6
Braden Mars <bradenmars@bradenmars.me>
2023-02-06 03:12:02 -0600

    feat(waybar): update waybar config, add updated weather script.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- dotfiles/config/waybar/modules/weather.old.sh
+++ dotfiles/config/waybar/modules/weather.old.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-# wttr.in's location service is a bit shit
-location=$1
-if [[ -z "$1" ]]; then
-	ifconfigco=$(curl -sS ifconfig.co/json)
-	city=$(echo $ifconfigco | jq ".city")
-	country_code=$(echo $ifconfigco | jq ".country_iso")
-
-	location=$(echo $city,$country_code | sed --expression "s/\"//g")
-fi
-
-# Set up caching to avoid tons of reqs to wttr
 cachedir=~/.cache/rbn
-cachefile=${0##*/}-$location
+cachefile=${0##*/}-$1
 
 if [ ! -d $cachedir ]; then
-	mkdir -p $cachedir
+    mkdir -p $cachedir
 fi
 
 if [ ! -f $cachedir/$cachefile ]; then
-	touch $cachedir/$cachefile
+    touch $cachedir/$cachefile
 fi
 
 # Save current IFS
@@ -29,10 +18,10 @@ IFS=$'\n'
 
 cacheage=$(($(date +%s) - $(stat -c '%Y' "$cachedir/$cachefile")))
 if [ $cacheage -gt 1740 ] || [ ! -s $cachedir/$cachefile ]; then
-	data=($(curl -s https://en.wttr.in/$location\?0qnT 2>&1))
-	echo ${data[0]} | cut -f1 -d, >$cachedir/$cachefile
-	echo ${data[1]} | sed -E 's/^.{15}//' >>$cachedir/$cachefile
-	echo ${data[2]} | sed -E 's/^.{15}//' >>$cachedir/$cachefile
+    data=($(curl -s https://en.wttr.in/$1\?0qnT 2>&1))
+    echo ${data[0]} | cut -f1 -d, > $cachedir/$cachefile
+    echo ${data[1]} | sed -E 's/^.{15}//' >> $cachedir/$cachefile
+    echo ${data[2]} | sed -E 's/^.{15}//' >> $cachedir/$cachefile
 fi
 
 weather=($(cat $cachedir/$cachefile))
@@ -40,48 +29,51 @@ weather=($(cat $cachedir/$cachefile))
 # Restore IFSClear
 IFS=$SAVEIFS
 
-temperature=$(echo ${weather[2]} | sed -E 's/ //g')
+temperature=$(echo ${weather[2]} | sed -E 's/([[:digit:]]+)\.\./\1 to /g')
+
+#echo ${weather[1]##*,}
 
 # https://fontawesome.com/icons?s=solid&c=weather
-# echo $(echo ${weather[1]##*,} | tr '[:upper:]' '[:lower:]')
 case $(echo ${weather[1]##*,} | tr '[:upper:]' '[:lower:]') in
 "clear" | "sunny")
-	icon=""
-	;;
+    condition=""
+    ;;
 "partly cloudy")
-	icon=""
-	;;
+    condition=""
+    ;;
 "cloudy")
-	icon=""
-	;;
+    condition=""
+    ;;
 "overcast")
-	icon=""
-	;;
+    condition=""
+    ;;
 "mist" | "fog" | "freezing fog")
-	icon=""
-	;;
+    condition=""
+    ;;
 "patchy rain possible" | "patchy light drizzle" | "light drizzle" | "patchy light rain" | "light rain" | "light rain shower" | "rain")
-	icon=""
-	;;
+    condition=""
+    ;;
 "moderate rain at times" | "moderate rain" | "heavy rain at times" | "heavy rain" | "moderate or heavy rain shower" | "torrential rain shower" | "rain shower")
-	icon=""
-	;;
+    condition=""
+    ;;
 "patchy snow possible" | "patchy sleet possible" | "patchy freezing drizzle possible" | "freezing drizzle" | "heavy freezing drizzle" | "light freezing rain" | "moderate or heavy freezing rain" | "light sleet" | "ice pellets" | "light sleet showers" | "moderate or heavy sleet showers")
-	icon=""
-	;;
+    condition=""
+    ;;
 "blowing snow" | "moderate or heavy sleet" | "patchy light snow" | "light snow" | "light snow showers")
-	icon=""
-	;;
+    condition=""
+    ;;
 "blizzard" | "patchy moderate snow" | "moderate snow" | "patchy heavy snow" | "heavy snow" | "moderate or heavy snow with thunder" | "moderate or heavy snow showers")
-	icon=""
-	;;
+    condition=""
+    ;;
 "thundery outbreaks possible" | "patchy light rain with thunder" | "moderate or heavy rain with thunder" | "patchy light snow with thunder")
-	icon=""
-	;;
+    condition=""
+    ;;
 *)
-	icon=""
-	# echo -e "{\"text\":\""\<span font=\'RobotoMono Nerd Font\'\>$icon\<\/span\>"\", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
-	;;
+    condition=""
+    echo -e "{\"text\":\""$condition"\", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
+    ;;
 esac
 
-echo -e "{\"text\":\""\<span font=\'Font Awesome 5 Free 10\'\>$icon\<\/span\>"  $temperature \", \"class\": \"weather\", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
+#echo $temp $condition
+
+echo -e "{\"text\":\""$temperature $condition"\", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
