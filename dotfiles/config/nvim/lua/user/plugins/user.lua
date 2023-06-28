2fd623d9827de25f5fb8c3c1b204d7384f8f16ba
Braden Mars <bradenmars@bradenmars.me>
2023-06-28 01:28:56 -0500

    feat(nvim): update nvim config
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ dotfiles/config/nvim/lua/user/plugins/user.lua
@@ -0,0 +1,16 @@
+return {
+     -- Suda plugin.
+    "lambdalisue/suda.vim",
+    -- Insert SheBang
+    {"sbdchd/vim-shebang", lazy = false },
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+}
